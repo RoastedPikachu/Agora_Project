1d8020feda469ed3b7c89413ba65fa7cd470728a
Boris Karabut <boriskarabytwork@gmail.com>
2024-02-01 11:24:37 +0300

    добавил кнопки регистрации на главную, а также начал писать стилевые классы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+
+import Link from "next/link";
+
+import Button from "@mui/material/Button";
+
+import {signIn} from "next-auth/react";
 
 import TheHomePageHeader from "@/widgets/TheHomePageHeader";
 
@@ -7,10 +14,28 @@ export default function Home() {
     <main className="relative w-full h-auto">
         <TheHomePageHeader/>
 
-        <section className="relative flex justify-center items-center w-full h-[750px]">
-          <img src="/static/ForestBackgroundImage.png" alt="" className="absolute w-full h-full brightness-[0.6] z-10"/>
+        <section className="relative px-[10%] w-full h-[755px] bg-[#2076d2]">
+          <h1 className="pt-[150px] w-full text-[#ffffff] text-[4rem] text-left font-['MerryWeather'] font-bold leading-[1.25] z-20"><strong className="text-[6rem] font-['Kamerik']">Agora</strong> <br/> New communication level</h1>
 
-          <h1 className="absolute w-full text-[#ffffff] text-[4rem] text-center font-['MerryWeather'] font-bold z-20"><strong className="text-[6rem] font-['Kamerik']">Agora</strong> <br/> New communication level</h1>
+          <p className="mt-[20px] text-[#ffffff] text-[2rem] font-['MerryWeather'] font-medium">The best corporate messanger which <br/> made by people and for people</p>
+
+          <div className="relative flex justify-between items-center mt-[30px] w-[70%] h-[50px]">
+              <Link href="/signUp" className="w-[26%] h-full">
+                  <Button variant="contained" className="w-full h-full bg-[#ffffff] text-[#2076d2] text-[1rem] hover:text-[#ffffff] font-bold">Sign up</Button>
+              </Link>
+
+              <Button variant="outlined" onClick={() => signIn('google')} className="flex justify-center items-center row-span-2 w-[35%] h-full bg-[#ffffff] border-2 rounded-[5px] text-[1rem] font-bold">
+                  <img src="/static/GoogleIcon.svg" alt="" className="w-[25px] h-[25px]"/>
+
+                  <p className="ml-[15px]">Sign Up with Google</p>
+              </Button>
+
+              <Button variant="contained" onClick={() => signIn('github')} className="row-span-2 w-[35%] h-full bg-[#000000] hover:bg-[#111111] border-2 hover:border-[2px] rounded-[5px] text-[1rem] font-bold">
+                  <img src="/static/GithubIcon.svg" alt="" className="ml-[-5px] w-[25px] h-[30px]"/>
+
+                  <p className="ml-[15px]">Sign Up with GitHub</p>
+              </Button>
+          </div>
         </section>                  
     </main>
   );
--- src/widgets/TheHomePageHeader.tsx
+++ src/widgets/TheHomePageHeader.tsx
@@ -6,16 +6,20 @@ import Button from "@mui/material/Button";
 
 const TheHomePageHeader = () => {
     return (
-        <header className="relative flex justify-between items-center pl-[15%] pr-[20px] w-full h-[45px] bg-[#ffffff]">
-           <h2 className="text-[#2076d2] text-[1.5rem] font-['Kamerik'] font-bold">Agora</h2>
+        <header className="relative flex justify-between items-center pl-[10%] pr-[50px] w-full h-[50px] bg-[#2076d2]">
+           <div className="flex items-center w-[15%]">
+               <img src="/static/AgoraLogo.svg" alt="" className="w-[25px] h-[25px]"/>
+
+               <h2 className="ml-[15px] text-[#ffffff] text-[1.5rem] font-['Kamerik'] font-bold">Agora</h2>
+           </div>
 
            <nav className="flex justify-between items-center w-[15%]">
-               <Link href="/signIn" className="">
-                   <Button variant="text">Sign in</Button>
+               <Link href="/signIn">
+                   <Button variant="text" className="textMUIWhiteButton">Sign in</Button>
                </Link>
 
-               <Link href="/signUp" className="">
-                  <Button variant="contained" className="bg-[#2076d2]">Sign up</Button>
+               <Link href="/signUp">
+                  <Button variant="contained" className="containedMUIWhiteButton">Sign up</Button>
                </Link>
            </nav>
         </header>
