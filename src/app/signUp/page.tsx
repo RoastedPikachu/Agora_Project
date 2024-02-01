56cf5b48b4a508781caf17d48f485755d891310f
Boris Karabut <boriskarabytwork@gmail.com>
2024-02-01 23:24:39 +0300

    адаптировал страницу регистрации под экраны мобильных устройств

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react';
 
+import {useIsMobileCheck} from "../../../customHooks";
+
 import Link from "next/link";
 
 import {signIn} from "next-auth/react";
@@ -9,27 +11,29 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 const Page = () => {
+    const isMobile = useIsMobileCheck();
+
     return (
         <main className="relative flex justify-center items-center w-full h-[100vh]">
-            <Link href="/" className="absolute top-[30px] left-[30px]">
+            <Link href="/" className="absolute top-[30px] mlarge:top-[20px] left-[30px] mlarge:left-[10px]">
                 <Button variant="text">
                     <img src="/static/LeftArrowIcon.svg" alt=""/>
 
-                    <p className="ml-[15px]">I don&lsquo;t want to sign up</p>
+                    {!isMobile && <p className="ml-[15px]">I don&lsquo;t want to sign up</p>}
                 </Button>
             </Link>
 
-            <form className="relative w-[40%] h-auto">
+            <form className="relative w-[40%] mlarge:w-[90%] h-auto">
                 <div className="relative grid justify-items-center grid-rows-[10] grid-cols-1 gap-y-[25px] w-full h-full">
-                    <h2 className="text-[#2076d2] text-[2.375rem] text-center font-bold">Insert you e-mail and password</h2>
+                    <h2 className="text-[#2076d2] text-[2.375rem] mmedium:text-[2.25rem] msmall:text-[2.125rem] text-center font-bold">Insert you e-mail and password</h2>
 
-                    <p className="text-[#2076d2] text-[1.375rem] text-center font-medium">We recommend to use work e-mail</p>
+                    <p className="text-[#2076d2] text-[1.375rem] mmedium:text-[1.125rem] msmall:text-[1rem] text-center font-medium">We recommend to use work e-mail</p>
 
-                    <TextField label="Insert E-mail" className="relative mx-[15%] w-[70%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
+                    <TextField label="Insert E-mail" className="relative mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
 
-                    <TextField label="Insert password" className="relative mx-[15%] w-[70%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
+                    <TextField label="Insert password" className="relative mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
 
-                    <Button variant="contained" className="flex justify-center items-center mx-[15%] w-[70%] h-[50px] bg-[#2076d2] rounded-[5px] text-[1.25rem] font-bold">Continue</Button>
+                    <Button variant="contained" className="flex justify-center items-center mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] bg-[#2076d2] rounded-[5px] text-[1.25rem] font-bold">Continue</Button>
 
                     <p className="text-[#2076f2] text-[1.25rem] font-bold">OR</p>
 
