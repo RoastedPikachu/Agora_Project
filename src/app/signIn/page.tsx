7dbf8d9130f5433387d0be65a353c582a23993e8
Boris Karabut <boriskarabytwork@gmail.com>
2024-02-03 12:22:23 +0300

    Вынес форму аутентификации в отдельный компонент
    Сделано это было для большей вариативности и возможности реиспользования в последующем

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,28 +1,16 @@
 "use client";
-import React, {useState} from 'react';
+import React from 'react';
 
 import Link from "next/link";
 
-import { useSession, signIn, signOut } from 'next-auth/react';
-
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 
 import {useIsMobileCheck} from "../../../customHooks";
 
-import firebaseSignIn from "../../../firebase/auth/signIn";
+import AuthForm from "@/features/AuthForm";
 
 const Page = () => {
     const isMobile = useIsMobileCheck();
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSignIn = async () => {
-        let result = await firebaseSignIn(email, password);
-
-        console.log(result);
-    }
 
     return (
         <main className="relative flex justify-center items-center w-full h-[100vh]">
@@ -33,38 +21,8 @@ const Page = () => {
                     {!isMobile && <p className="ml-[15px]">I don&lsquo;t want to sign in</p>}
                 </Button>
             </Link>
-            
-            <form className="relative w-[40%] mlarge:w-[90%] h-auto">
-                <div className="relative grid justify-items-center grid-rows-[10] grid-cols-1 gap-y-[25px] w-full h-full">
-                    <h2 className="text-[#2076d2] text-[2.375rem] mmedium:text-[2.25rem] msmall:text-[2.125rem] text-center font-bold">Sign in to Agora</h2>
 
-                    <p className="text-[#2076d2] text-[1.375rem] mmedium:text-[1.125rem] msmall:text-[1rem] text-center font-medium">We recommend to use work e-mail</p>
-
-                    <Button variant="outlined" onClick={() => signIn('google')} className="googleOAuthButton">
-                        <img src="/static/GoogleIcon.svg" alt="" className="w-[25px] h-[25px]"/>
-
-                        <p className="ml-[15px]">Continue with Google</p>
-                    </Button>
-
-                    <Button variant="contained" onClick={() => signIn('github')} className="githubOAuthButton">
-                        <img src="/static/GithubIcon.svg" alt="" className="ml-[-5px] w-[25px] h-[30px]"/>
-
-                        <p className="ml-[15px]">Continue with GitHub</p>
-                    </Button>
-
-                    <p className="text-[#2076d2] text-[1.25rem] font-bold">OR</p>
-
-                    <TextField type="text" label="Insert E-mail" onChange={(event) => setEmail(event.target.value)} className="authInputMUIField"/>
-
-                    <TextField type="password" label="Insert password" onChange={(event) => setPassword(event.target.value)} className="authInputMUIField"/>
-
-                    <Button variant="contained" onClick={() => handleSignIn()} className="flex justify-center items-center mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] bg-[#2076d2] rounded-[5px] text-[1.25rem] font-bold">Sign in</Button>
-
-                    <Link href="/signUp">
-                        <Button variant="text">Create an account</Button>
-                    </Link>
-                </div>
-            </form>
+            <AuthForm isSignIn={true} authHeader={"Sign in to Agora"} authDescription={"We recommend to use work e-mail"} otherChoiseText={"Create an account"}/>
         </main>
     );
 };
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,28 +1,16 @@
 "use client";
-import React, {useState} from 'react';
+import React from 'react';
 
 import Link from "next/link";
 
-import {signIn} from "next-auth/react";
-
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 
 import {useIsMobileCheck} from "../../../customHooks";
 
-import firebaseSignUp from "../../../firebase/auth/signUp";
+import AuthForm from "@/features/AuthForm";
 
 const Page = () => {
     const isMobile = useIsMobileCheck();
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSignUp = async () => {
-        let result = await firebaseSignUp(email, password);
-
-        console.log(result);
-    }
 
     return (
         <main className="relative flex justify-center items-center w-full h-[100vh]">
@@ -34,37 +22,12 @@ const Page = () => {
                 </Button>
             </Link>
 
-            <form className="relative w-[40%] mlarge:w-[90%] h-auto">
-                <div className="relative grid justify-items-center grid-rows-[10] grid-cols-1 gap-y-[25px] w-full h-full">
-                    <h2 className="text-[#2076d2] text-[2.375rem] mmedium:text-[2.25rem] msmall:text-[2.125rem] text-center font-bold">Insert you e-mail and password</h2>
-
-                    <p className="text-[#2076d2] text-[1.375rem] mmedium:text-[1.125rem] msmall:text-[1rem] text-center font-medium">We recommend to use work e-mail</p>
-
-                    <TextField type="text" label="Insert E-mail" onChange={(event) => setEmail(event.target.value)} className="relative mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
-
-                    <TextField type="password" label="Insert password" onChange={(event) => setPassword(event.target.value)} className="relative mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] border-2 rounded-[5px] placeholder:text-[#120154]"/>
-
-                    <Button variant="contained" onClick={() => handleSignUp()} className="flex justify-center items-center mx-[15%] mlarge:mx-[5%] w-[70%] mlarge:w-[80%] h-[50px] bg-[#2076d2] rounded-[5px] text-[1.25rem] font-bold">Continue</Button>
-
-                    <p className="text-[#2076f2] text-[1.25rem] font-bold">OR</p>
-
-                    <Button variant="outlined" onClick={() => signIn('google')} className="googleOAuthButton">
-                        <img src="/static/GoogleIcon.svg" alt="" className="w-[25px] h-[25px]"/>
-
-                        <p className="ml-[15px]">Continue with Google</p>
-                    </Button>
-
-                    <Button variant="contained" onClick={() => signIn('github')} className="githubOAuthButton">
-                        <img src="/static/GithubIcon.svg" alt="" className="ml-[-5px] w-[25px] h-[30px]"/>
-
-                        <p className="ml-[15px]">Continue with GitHub</p>
-                    </Button>
-
-                    <Link href="/signIn">
-                        <Button variant="text">Sign in instead</Button>
-                    </Link>
-                </div>
-            </form>
+            <AuthForm
+                isSignIn={false}
+                authHeader={"Insert you e-mail and password"}
+                authDescription={"We recommend to use work e-mail"}
+                otherChoiseText={"Sign in instead"}
+            />
         </main>
     );
 };
