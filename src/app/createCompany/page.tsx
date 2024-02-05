c4554f4ca0ff9ed762b171fba2ce2cc28f9845fd
Boris Karabut <boriskarabytwork@gmail.com>
2024-02-05 14:31:03 +0300

    Добавил функцию обработки загружаемого аватара компании

--- src/app/createCompany/page.tsx
+++ src/app/createCompany/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
 
 import GoBackButton from "@/shared/GoBackButton";
 
 const Page = () => {
+    const [companyAvatar, setCompanyAvatar] = useState("");
+
+    const handleImageLoad = (event:any) => {
+          setCompanyAvatar(URL.createObjectURL(event.target.files[0]));
+    }
+
     return (
         <main className="relative flex justify-center items-center w-full h-[100vh]">
             <GoBackButton designation={"/signUp"} buttonText={"I don't want to create company"}/>
@@ -11,7 +18,7 @@ const Page = () => {
                 <div className="relative w-full h-[100px]">
                     <label className="w-[100px] h-[100px] bg-[#747474]"></label>
 
-                    <input type="file" className="hidden"/>
+                    <input type="file" onChange={(event) => handleImageLoad(event)} className="hidden"/>
                 </div>
             </form>
         </main>
