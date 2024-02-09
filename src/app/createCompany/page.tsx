10437af906ad8133a0f349ba68ba4b5b06ecbf52
Boris Karabut <boriskarabytwork@gmail.com>
2024-02-09 08:46:49 +0300

    Не помню что делал в этом коммите

--- src/app/createCompany/page.tsx
+++ src/app/createCompany/page.tsx
@@ -16,9 +16,17 @@ const Page = () => {
 
             <form className="grid justify-items-center w-[50%] h-auto">
                 <div className="relative w-full h-[100px]">
-                    <label className="w-[100px] h-[100px] bg-[#747474]"></label>
+                    <div className="relative flex w-full">
+                        <div className="relative px-[20px] w-[260px] h-auto">
+                            <label htmlFor="CompanyAvatar" className="flex justify-center items-center mx-[10px] pb-[20px] w-[200px] h-[200px] border-4 border-dashed border-[#2076d2] rounded-[10px] text-[#2076d2] text-[8rem] cursor-pointer">+</label>
 
-                    <input type="file" onChange={(event) => handleImageLoad(event)} className="hidden"/>
+                            <p className="mt-[20px] w-full text-[#2076d2] text-[1rem] text-center font-['Kamerik']">Upload company avatar</p>
+                        </div>
+
+                        <h2 className="ml-[10px] text-[#2076d2] text-[2.5rem] font-['Kamerik']">Create your <br/> company</h2>
+                    </div>
+
+                    <input type="file" onChange={(event) => handleImageLoad(event)} id="CompanyAvatar" className="hidden"/>
                 </div>
             </form>
         </main>
